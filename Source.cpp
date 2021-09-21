0157fe70357bf43affc6a0be50a17d1fae3d189b
Mantulis00 <mantas.radzevicius1@gmail.com>
2021-09-21 21:20:00 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,88 +5,97 @@
 #include <time.h>  
 
 #define N 20
+#define NBrute 3
+#define epsilon 0.01
 
 
-double* InputWeights(int n)
-{
-    static double weights[N];
 
-    for (int x = 0; x < n; x++)
-    {
-        std::cin >> weights[x];
-    }
-    
-    return weights;
-}
-
-double* GenerateInputs()
-{
-    static double inputs[N];
-
-    for (int x = 0; x < N; x++)
-    {
-        inputs[x] = (rand() % 20000) / 10000.0 - 1;
-    }
-    return inputs;
-}
-
-double Sigmoid(double weight)
+double Sigmoid(double weight) // returns sigmoid activation result 
 {
     double e = 2.718;
     return (1 / (1+pow(e, -weight)));
 }
 
-double Threshold(double weight)
+double Threshold(double weight) // returns threshold activation result
 {
     double threshold = 0;
     if (weight >= threshold) return 1;
     return 0;
 }
 
-double GetWeight(int n, double w0, double* weights, double* inputs)
+
+
+bool CheckConditionThreshold(double* inputs, double* weights, double* results)
 {
-    double weight = w0;
-    for (int x = 0; x < n; x++)
+    int count = 4;
+    for (int x = 0; x < count; x ++) // 0 3 6 9
     {
-        weight += inputs[x] * weights[x];
-        std::cout << '\n' << "Weight: " << inputs[x] << " * " << weights[x] << '\n';
+        int i = x * NBrute;
+        double wa = inputs[0] * weights[i] + inputs[1] * weights[(i + 1)] + inputs[2] * weights[(i + 2)];
+        double a = Threshold(wa);
+        double r = results[x];
+        if (abs(a - r) > epsilon)
+            return false;
     }
-    return weight;
+    return true;
 }
 
-void PrintInputs(int n, double* weights, double* inputs)
+bool CheckConditionSigmoid(double* inputs, double* weights, double* results)
 {
-    std::cout << '\n' << "Inputs: " << '\n' << '\n';
-
-    std::cout << "w" << 0 << " = " << 0 << " x" << 0 << " = " << 1 << '\n';
-    for (int x = 0; x < n; x++)
+    int count = 4;
+    for (int x = 0; x < count; x++) // 0 3 6 9
     {
-        std::cout << "w" << x+1 << " = " << weights[x] << " x" << x+1 << " = " << inputs[x] << '\n';
+        int i = x * NBrute;
+        double wa = inputs[0] * weights[i] + inputs[1] * weights[(i + 1)] + inputs[2] * weights[(i + 2)];
+        double a = Sigmoid(wa);
+        double r = results[x];
+        if (abs(a - r) > epsilon)
+            return false;
+    }
+    return true;
+}
+
+
+void BruteThreshold(double* weights, double* results)
+{
+    bool threshFound = false, sigmoidFound = false;
+    for (double w0 = -30; w0 < 30; w0+=0.1)
+    {
+        //std::cout << '\n' << w0 / 100 << '\n';
+        for (double w1 = -30; w1 < 30; w1+=0.1)
+        {
+            for (double w2 = -30; w2 < 30; w2+=0.1)
+            {
+                double inputs[NBrute] = {w0,w1,w2 };
+                //std::cout <<  "w0 " << w0 << "w1 " << w1 << "w2 " << w2 << '\n';
+                if (CheckConditionThreshold(inputs, weights, results) && !threshFound)
+                {
+                    std::cout << "Correct Threshold weights: " << '\n' << "w0 =" << w0 << '\n' << " " << "w1 = " << w1 << '\n' << " " << "w2 " << w2 << '\n';
+                    threshFound = true;
+                }
+                if (CheckConditionSigmoid(inputs, weights, results) && !sigmoidFound)
+                {
+                    std::cout <<'\n' << "Correct Sigmoid weights: " << '\n' << "w0 =" << w0 << '\n' << " " << "w1 = " << w1 << '\n' << " " << "w2 " << w2 << '\n';
+                    sigmoidFound = true;
+                }
+                if (sigmoidFound && threshFound) return;
+            }
+        }
     }
 }
 
-void PrintResults(double weight)
+void BruteSigmoid(double weight, int count)
 {
-    std::cout << '\n' << "Results: " << '\n' ;
-    std::cout << '\n' << "Weight: " << weight << '\n';
-    std::cout << '\n' << "Sigmoid: " << Sigmoid(weight) << '\n';
-    std::cout << '\n' << "Threshold: " << Threshold(weight) << '\n';
+
 }
 
 
 
 int main()
 {
-    int n;
-    std::cin >> n;
 
-    double* inputs = GenerateInputs();
-    double* weights;
-    weights = InputWeights(n);
+    double inputs[N] = { 1, -0.2, 0.5, 1, 0.2, -0.5, 1, 0.8, -0.8, 1, 0.8, 0.8 };// GenerateInputs(n);
+    double results[4] = { 0, 0, 1, 1 };
 
-    double w0 = 1;
-    double weight = GetWeight(n, w0, weights, inputs);
-
-    PrintInputs(n, weights, inputs);
-    PrintResults(weight);
+    BruteThreshold(inputs, results);
 }
